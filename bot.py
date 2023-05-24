526fcbb02f7ec21a7a34115bbbe8875cca9be503
Ian Paolo Liberal <ianpaololiberal@gmail.com>
2023-05-24 15:54:06 +0300

    config parsing and event registering

--- bot.py
+++ bot.py
@@ -1,11 +1,25 @@
 import discord
+from utils.config import prefix, log_channel, log_level
+
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+bot = discord.Client(intents=intents)
 
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    
+    # Find the log channel from every server the bot is in
+    for guild in bot.guilds:
+        for channel in guild.text_channels:
+            # Sends a "Bot ready." message to the log channel
+            if channel.name == log_channel:
+                message = "Bot ready."
+                await channel.send(message)
 
 
 
 
 token = open(".env/token", "r").read()
-client.run(token)
+bot.run(token)
---
+++ utils/config.py
@@ -0,0 +1,20 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+
+# Discord
+prefix = config.get('Discord', 'prefix').split(", ")
+admin_role = config.get('Discord', 'admin_role')
+
+# APIs
+
+
+#Logging
+log_channel = config.get('Logging', 'log_channel')
+log_level = [
+    eval(config.get('Logging', 'log_debug')),       # debug
+    eval(config.get('Logging', 'log_info')),        # info
+    eval(config.get('Logging', 'log_warnings')),    # warnings
+    eval(config.get('Logging', 'log_errors'))       # errors
+]
