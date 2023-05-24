8742fd1654e721ec4482ff7b00a1d92708aa5601
Lassi Chen <lachen@utu.fi>
2023-05-24 20:59:36 +0300

    Coin Flip implemented

---
+++ modules/flip.py
@@ -0,0 +1,17 @@
+from random import choice
+import discord
+
+async def main(message):
+    # Random Choice
+    flip = choice(["Heads", "Tails"])
+    
+    # Builds Embed
+    embed = discord.Embed(
+        title = flip
+    )
+    if flip == "Heads":
+        embed.set_image(url="https://cdn.discordapp.com/attachments/1087535765954777180/1110984570066714745/Heads.png")
+    elif flip == "Tails":
+        embed.set_image(url="https://cdn.discordapp.com/attachments/1087535765954777180/1110985253205573672/Tails.png")
+
+    await message.channel.send(embed=embed)
