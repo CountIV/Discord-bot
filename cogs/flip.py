f32b8b5aafaf783dee7fce217b2bae092382b74f
Lassi Chen <lachen@utu.fi>
2023-05-25 22:24:34 +0300

    flip reimplemented

--- cogs/flip.py
+++ cogs/flip.py
@@ -1,19 +1,27 @@
 from random import choice
 import discord
+from discord.ext import commands
 
-description = """Flips a coin."""
+class flip(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.description = description = """Flips a coin"""
 
-async def main(message):
-    # Random Choice
-    flip = choice(["Heads", "Tails"])
-    
-    # Builds Embed
-    embed = discord.Embed(
-        title = flip
-    )
-    if flip == "Heads":
-        embed.set_image(url="https://cdn.discordapp.com/attachments/1087535765954777180/1110984570066714745/Heads.png")
-    elif flip == "Tails":
-        embed.set_image(url="https://cdn.discordapp.com/attachments/1087535765954777180/1110985253205573672/Tails.png")
+    @commands.command()
+    async def flip(self, ctx):
+        # Random Choice
+        flip = choice(["Heads", "Tails"])
+        
+        # Builds Embed
+        embed = discord.Embed(
+            title = flip
+        )
+        if flip == "Heads":
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1087535765954777180/1110984570066714745/Heads.png")
+        elif flip == "Tails":
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1087535765954777180/1110985253205573672/Tails.png")
 
-    await message.channel.send(embed=embed)
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(flip(bot))
