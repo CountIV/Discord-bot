364934a962b996cdf9dd69df157f0c216e4093d3
Lassi Chen <lachen@utu.fi>
2023-05-26 22:01:59 +0300

    added functionality to ping other users

--- cogs/ping.py
+++ cogs/ping.py
@@ -5,9 +5,15 @@ class ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(help='- Tests server ping')
-    async def ping(self, ctx):
-        await ctx.send(f"Ping is {round(self.bot.latency * 1000)}ms")
+    @commands.command()
+    async def ping(self, ctx, *, user: discord.Member=None):
+        """- Tests server ping"""
+        if user == None:
+            await ctx.send(f"Ping is {round(self.bot.latency * 1000)}ms")
+            return
+        
+        """- Alternative: Ping someone"""
+        await ctx.send(user.mention)
 
 async def setup(bot):
     await bot.add_cog(ping(bot))
