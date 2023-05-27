03a94b418b284823d6cd896553e7f455c8a0696e
Ian Paolo Liberal <ianpaololiberal@gmail.com>
2023-05-27 15:33:06 +0300

    Update roll.py

--- cogs/roll.py
+++ cogs/roll.py
@@ -10,20 +10,14 @@ class Roll(commands.Cog):
 
 
     @commands.command()
-    async def roll(self, ctx, roll=""):
+    async def roll(self, ctx, roll="1d6"):
         """- Rolls dice of the form [number]d[number]"""
 
-        # If the arg is empty, assume 1d6
-        if roll == "" or roll == "1d6":
-            embed = discord.Embed(title = f"{randint(1, 6)}")
-            await ctx.send(embed=embed)
-            return
-
         # Search for the roll input with regex
-        roll = re.search("[0-9]+d[0-9]+", roll)
+        pattern = re.search("[0-9]+d[0-9]+", roll)
         
         # If the input is invalid, return an error message
-        if roll == None:
+        if pattern == None:
             embed = discord.Embed(
             title = "Invalid input",
             description = f"This command accepts inputs of the form [number]d[number], \nwhere the first number is the amound of dice you want to roll and the second number is the amound of sides on the dice.",
@@ -34,7 +28,7 @@ class Roll(commands.Cog):
             return
         
         # Isolate the roll input
-        roll = roll.group()
+        roll = pattern.group()
 
         # Split the roll input
         dice_count, dice_size = roll.split("d")
@@ -43,18 +37,37 @@ class Roll(commands.Cog):
         dice_count = int(dice_count)
         dice_size = int(dice_size)
 
+        if dice_size * dice_count > 10000000:
+            embed = discord.Embed(title = f"Input too large")
+            await ctx.send(embed=embed)
+            return
+
         # Determine roll values
         calculated_rolls = [randint(1, dice_size) for _ in range(dice_count)]
         total = sum(calculated_rolls)
 
-        # Convert to string and join with an empty space for readability
-        individual_rolls = " ".join([str(i) for i in calculated_rolls])
-        embed = discord.Embed(
+        # If there is only 1 dice, roll only one
+        if roll[0:1] == "1d" and roll[2:].isnumeric():
+            embed = discord.Embed(title = f"{randint(1, int(roll[2:]))}")
+            await ctx.send(embed=embed)
+            return
+        
+        # Convert to string with formatted leading zeroes and join with an empty space
+        individual_rolls = " ".join([str(f"%0{len(str(dice_size))}d" % i) for i in calculated_rolls])
+        # Change format depending on dice count
+        if dice_count == 1:
+            embed = discord.Embed(title=f"{total}")
+        elif dice_count * len(str(dice_size)) > 4000:
+            embed = discord.Embed(
             title = f"Rolling {dice_count}x {dice_size}-sided dice",
-            description = f"Individual rolls: \n{individual_rolls}\nTotal: {total}",
-            color = 16777215
+            description=f"Total: **{total}**"
         )
-
+        else:
+            embed = discord.Embed(
+                title = f"Rolling {dice_count}x {dice_size}-sided dice",
+                description = f"```haskell\n{individual_rolls}```\nTotal: **{total}**",
+            )
+        
         await ctx.send(embed=embed)
 
 
