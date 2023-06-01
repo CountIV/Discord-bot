90f9e6973895c93919ad3d67243bba8f9c2cb4ab
Lassi Chen <lachen@utu.fi>
2023-06-01 09:57:22 +0300

    index fix

--- cogs/ciphers.py
+++ cogs/ciphers.py
@@ -1,27 +1,27 @@
 import discord
 from discord.ext import commands
 
-class Cipher(commands.Cog):
+class Ciphers(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(help='- Applies Ceasar cipher to the message')
-    async def cipher(self, ctx, *, message):
-        # Ciphers one letter at time
+    @commands.command()
+    async def caesar(self, ctx, *, message):
+        """ - Applies Ceasar cipher to the message """
         ciphered_message = ""
         for letter in message:
             ascii_val = ord(letter)
             if letter.isupper() and ascii_val >= 65 and ascii_val <= 90:
-                ciphered_letter = chr((ascii_val - 65 - 3) % 26 + 65)                
+                ciphered_letter = chr((ascii_val - 65 - 3) % 26 + 65)
             elif letter.islower() and ascii_val >= 97 and ascii_val <= 122:
                 ciphered_letter = chr((ascii_val - 97 - 3) % 26 + 97)
             else:
                 ciphered_letter = letter
             ciphered_message += ciphered_letter
-        
-        await ctx.send(ciphered_message)            
+
+        await ctx.send(ciphered_message)
 
 
 
 async def setup(bot):
-    await bot.add_cog(Cipher(bot))
+    await bot.add_cog(Ciphers(bot))
