c91aa00e5b8c887e12b67e4519e0f02590d8fbd8
Ian Paolo Liberal <ianpaololiberal@gmail.com>
2023-06-02 15:38:06 +0300

    added reloading of cogs without restart

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 import discord
 import os
 from discord.ext import commands
-from utils.config import prefix
+from utils.config import prefix, admin_role
 
 
 # Create bot object
 bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
 
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
-
     # List of cog files within the cogs folder
     cog_files = [f for f in os.listdir('cogs') if f.endswith('.py')]
     
@@ -22,6 +21,9 @@ async def on_ready():
             await bot.load_extension(extension)
         except Exception as e:
             print(e)
+    
+    print(f'Logged in as {bot.user}')
+
 
 
 @bot.event
@@ -29,6 +31,28 @@ async def on_member_join(member):
     # Gives warm welcome to new members
     channel = member.guild.system_channel
     await channel.send(f"{member.mention} blasts into the server!")
+
+
+
+@bot.command()
+@commands.has_role(admin_role)
+async def reload_cogs(ctx):
+    """- Reloads all cogs"""
+    cog_files = [f for f in os.listdir('cogs') if f.endswith('.py')]
+    for cog in cog_files:
+        extension = f"cogs.{cog[:-3]}"
+        try:
+            await bot.reload_extension(extension)
+        except Exception as e:
+            try:
+                await bot.load_extension(extension)
+            except Exception as e:
+                print(e)
+            print(e)
+
+    embed = discord.Embed(description=f"All cogs have been reloaded.")
+    await ctx.send(embed=embed)
+
 
 
 if __name__ == "__main__":
