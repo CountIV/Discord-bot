978cca08eeb94aaa0d475b1736063597df1649ce
Ian Paolo Liberal <ianpaololiberal@gmail.com>
2023-06-07 13:48:34 +0300

    chess utils

--- utils/chess.py
+++ utils/chess.py
@@ -1,4 +1,66 @@
-class Pawn:
+class Piece:
+    movement_pattern = [
+        (0, 0)
+    ]
+
+
+    @classmethod
+    def can_move(cls, old, new):
+        # Check if the new move is within bounds
+        for i in new:
+            if not 0 <= i <= 7:
+                return False
+
+        # Get the offset of the new move
+        difference = tuple(map(lambda x, y: y - x, old, new))
+
+        if difference in cls.movement_pattern:
+            return True
+
+        return False
+
+
+
+class King(Piece):
+    movement_repeats = False
+    movement_pattern = [
+        (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)    
+    ]
+
+
+
+class Queen(Piece):
+    movement_repeats = True
+    movement_pattern = [
+        (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) 
+    ]
+
+
+class Rook(Piece):
+    movement_repeats = True
+    movement_pattern = [
+        (1, 0), (0, 1), (-1, 0), (0, -1)
+    ]
+
+
+
+class Bishop(Piece):
+    movement_repeats = True
+    movement_pattern = [
+        (1, 1), (1, -1), (-1, 1), (-1, -1)
+    ]
+
+
+
+class Knight(Piece):
+    movement_repeats = False
+    movement_pattern = [
+        (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)
+    ]
+
+
+
+class Pawn(Piece):
     movement_repeats = False
     movement_pattern = [
         (0, 1)
@@ -8,49 +70,18 @@ class Pawn:
     ]
 
 
+    @classmethod
     def can_move(self, old, new, initial_move=False):
+        movement_pattern = [(0, 1), (0, 2)] if initial_move else self.movement_pattern
         # Check if the new move is within bounds
         for i in new:
-            if not 1 <= i <= 8:
+            if not 0 <= i <= 7:
                 return False
-
-        if initial_move:
-            Pawn.movement_pattern = [
-                (0, 1), (0, 2)
-            ]
-        else:
-            Pawn.movement_pattern = [
-                (0, 1)
-            ]
-
+        
         # Get the offset of the new move
-        difference = tuple(map(lambda x, y: x - y, old, new))
+        difference = tuple(map(lambda x, y: y - x, old, new))
 
-        if difference in Pawn.movement_pattern:
-            return True
-
-        return False
-
-
-
-class Knight:
-    movement_repeats = False
-    movement_pattern = [
-        (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)
-    ]
-    attack_pattern = movement_pattern
-
-
-    def can_move(self, old, new):
-        # Check if the new move is within bounds
-        for i in new:
-            if not 1 <= i <= 8:
-                return False
-
-        # Get the offset of the new move
-        difference = tuple(map(lambda x, y: x - y, old, new))
-
-        if difference in Knight.movement_pattern:
+        if difference in movement_pattern:
             return True
 
         return False
