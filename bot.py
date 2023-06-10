c92ec946e41493d6f23a2486c64e36fc58dd231f
Ian Paolo Liberal <ianpaololiberal@gmail.com>
2023-06-10 22:11:37 +0300

    enhanced feature to reload all cogs

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import time
 import discord
 import os
 from discord.ext import commands
@@ -36,9 +37,13 @@ async def on_member_join(member):
 
 @bot.command()
 @commands.has_role(admin_role)
-async def reload_cogs(ctx):
-    """- Reloads all cogs"""
+async def restart(ctx):
+    """Restarts all cogs"""
+    start_time = time.time()
+    embed = discord.Embed(title=f"Restarting...", color=discord.Color.red())
+    waiting = await ctx.send(embed=embed)
     cog_files = [f for f in os.listdir('cogs') if f.endswith('.py')]
+    errors = ""
     for cog in cog_files:
         extension = f"cogs.{cog[:-3]}"
         try:
@@ -47,10 +52,16 @@ async def reload_cogs(ctx):
             try:
                 await bot.load_extension(extension)
             except Exception as e:
-                print(e)
+                errors += f"{e}\n"
 
-    embed = discord.Embed(description=f"All cogs have been reloaded.")
-    await ctx.send(embed=embed)
+    embed = discord.Embed(title       = f"**{bot.user.name}** has been rebooted", color=discord.Color.green(), 
+                          description = f"```yaml\n{errors}```" if errors else "")
+
+    elapsed_time = time.time() - start_time
+    elapsed_time_formatted = f"{elapsed_time:.2f}s"
+    embed.set_footer(text=f"Boot time: {elapsed_time_formatted}")
+
+    await waiting.edit(embed=embed)
 
 
 
