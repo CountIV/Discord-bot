c5c301c46bf831bfb57be3a530cb66f8c907e925
Lassi Chen <lachen@utu.fi>
2023-06-21 05:23:41 +0300

    Update weather is now embed and Add sunrise & sunset

--- cogs/weather.py
+++ cogs/weather.py
@@ -1,20 +1,23 @@
+import discord
 from discord.ext import commands
 import requests
+from datetime import datetime
 
 class Weather(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(aliases=["w"])
-    async def weather(self, ctx, *, city):
+    async def weather(self, ctx, *, message):
         """Gives current temperature and weather of given city"""
         # API configuration
         api_key = open(".env/weather_api_key", "r").read()
         base_url = "http://api.openweathermap.org/"
 
         # Get location coordinates first
-        geo_url = base_url + "geo/1.0/direct" + "?q=" + city + "&appid=" + api_key
+        geo_url = base_url + "geo/1.0/direct" + "?q=" + message + "&appid=" + api_key
         response1 = requests.get(geo_url).json()[0]
+        city = response1["name"]
         lat = str(response1["lat"])
         lon = str(response1["lon"])
         unit = "metric"
@@ -24,10 +27,23 @@ class Weather(commands.Cog):
         response2 = requests.get(weather_url).json()
 
         # Get relevant data from requested json
-        temp = response2["main"]["temp"]
-        weather = response2["weather"][0]["description"]
+        weather = response2["weather"][0]["main"]
+        temperature = str(int(response2["main"]["temp"])) + "°C"
 
-        await ctx.send(f"Temperature is {temp} and weather is {weather}")
+        utc_offset = (datetime.now() - datetime.utcnow()).seconds
+        local_time_offset = response2["timezone"]
+        sunrise = datetime.fromtimestamp(response2["sys"]["sunrise"] - utc_offset + local_time_offset).strftime("%H:%M")
+        sunset = datetime.fromtimestamp(response2["sys"]["sunset"] - utc_offset + local_time_offset).strftime("%H:%M")
+
+        embed = discord.Embed(title=city, color=0x1aa9cd)
+        embed.add_field(name="Weather", value=weather)
+        embed.add_field(name="Temperature", value=temperature)
+        embed.add_field(name="", value="")
+        embed.add_field(name="Sunrise", value=sunrise)
+        embed.add_field(name="Sunset", value=sunset)
+        embed.add_field(name="", value="")
+
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Weather(bot))
