7ef6046e71a956a2cf20518e190004437421a423
Lassi Chen <lachen@utu.fi>
2023-07-07 15:58:40 +0300

    Add simple calculator (only sum)

---
+++ cogs/calculator.py
@@ -0,0 +1,31 @@
+from random import choice
+import discord
+from discord.ext import commands
+
+class Calculator(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["cal"])
+    async def calculator(self, ctx, *, message):
+        """Math calculator"""
+        calc_list = []
+        j = 0
+
+        for i, l in enumerate(message):
+            if l == "+":
+                calc_list.append(int(message[j:i]))
+                calc_list.append(message[i])
+                j = i+1
+        calc_list.append(int(message[j:]))
+
+        result = calc_list[0]
+        for i, l in enumerate(calc_list):
+            if l == "+":
+                result += calc_list[i+1]
+
+        await ctx.send(result)
+
+
+async def setup(bot):
+    await bot.add_cog(Calculator(bot))
